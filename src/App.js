04a0a855f657060783a6a64d6e621881953121cf
AjithGWC <Ajith.Kumar@gwcdata.ai>
2024-07-20 16:58:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './dist/images/logo.svg';
 import './App.css';
+import './dist/css/app.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav class="side-nav">
+      <a href="" class="intro-x flex items-center pl-5 pt-4">
+        <img alt="Rubick Tailwind HTML Admin Template" class="w-6" src={logo}></img>
+        <span class="hidden xl:block text-white text-lg ml-3"> Ticketing Tool </span> 
+      </a>
+      <ul class="fd-c">   
+        <li class="p-0 mt-6 ml-0" id="nav-home">
+          <a href="" class="side-menu side-menu--active">
+            <div class="side-menu__icon"> <i data-feather="credit-card"></i> </div>
+            <div class="side-menu__title"> Ticket Manager</div>
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Main from "./main"
+import './dist/css/app.css';
 import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="flex">
+      <App />
+      <Main />
+    </div>
+    
   </React.StrictMode>
 );
 
