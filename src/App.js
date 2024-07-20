8b8b480309747fbde426ba9692081c2bb88202b1
AjithGWC <Ajith.Kumar@gwcdata.ai>
2024-07-20 17:39:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import './dist/css/app.css';
 function App() {
   return (
     <nav class="side-nav">
-      <a class="intro-x flex items-center pl-5 pt-4"> 
+      <a href='#' class="intro-x flex items-center pl-5 pt-4">
         <img alt="Rubick Tailwind HTML Admin Template" class="w-6" src={logo}></img>
         <span class="hidden xl:block text-white text-lg ml-3"> Ticketing Tool </span> 
       </a>
       <ul class="fd-c">   
         <li class="p-0 mt-6 ml-0" id="nav-home">
-          <a class="side-menu side-menu--active">
+          <a href='#' class="side-menu side-menu--active">
             <div class="side-menu__icon"> <i data-feather="credit-card"></i> </div>
             <div class="side-menu__title"> Ticket Manager</div>
           </a>
